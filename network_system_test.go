b3ca9e83abf299bcbb87e6a513630d1cadc91574
Richard Wohlbold <richard@rgwohlbold.de>
2023-05-12 16:24:52 +0200

    Fix tests

--- network_system_test.go
+++ network_system_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestVniToEsi(t *testing.T) {
-	esi := NewFRRClient().vniToEsi(100)
+	esi := NewSystemNetworkStrategy().vniToEsi(100)
 	result := "00:00:00:00:00:00:00:00:00:64"
 	if esi != result {
 		t.Errorf("VniToEsi(100) = %s; want %s", esi, result)
