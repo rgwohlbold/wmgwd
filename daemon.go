a59cd0bf8553fd370f3cc51bd59b0e2616806159
Richard Wohlbold <richard@rgwohlbold.de>
2023-05-19 12:48:49 +0200

    Delete event.go

--- daemon.go
+++ daemon.go
@@ -27,6 +27,20 @@ type Daemon struct {
 	db                   *Database
 }
 
+type EventIngestor[E any] interface {
+	Ingest(ctx context.Context, daemon *Daemon, eventChan chan<- E, setupChan chan<- struct{})
+}
+
+func runEventIngestor[E any](ctx context.Context, daemon *Daemon, ingestor EventIngestor[E], eventChan chan<- E, wg *sync.WaitGroup) {
+	wg.Add(1)
+	setupChan := make(chan struct{})
+	go func() {
+		ingestor.Ingest(ctx, daemon, eventChan, setupChan)
+		wg.Done()
+	}()
+	<-setupChan
+}
+
 func NewDaemon(config Configuration, ns NetworkStrategy, as AssignmentStrategy) *Daemon {
 	return &Daemon{
 		Config:               config,
